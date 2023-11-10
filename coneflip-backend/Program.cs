00f0f507b78fd2b5f02300dd23c709a07d41112d
unknown <mitch1080@hotmail.co.uk>
2023-11-10 18:56:57 +0000

    Add basic backend

---
+++ coneflip-backend/Program.cs
@@ -0,0 +1,65 @@
+using Microsoft.Data.Sqlite;
+using Dapper;
+using System.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors();
+builder.Services.AddScoped<IDbConnection>(_ =>
+    new SqliteConnection(builder.Configuration.GetConnectionString("Database")));
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// app.UseCors(x => x
+//     .AllowAnyMethod()
+//     .AllowAnyHeader()
+//     .WithOrigins(app.Configuration.GetSection("AllowedHosts").Get<string[]>()!)
+//     .AllowCredentials());
+
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .WithOrigins("http://127.0.0.1:5500")
+    .AllowCredentials());
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/gettopthree", (IDbConnection db) =>
+{
+   return db.Query<Coneflip>("SELECT [name],[score],[video_link] FROM [coneflip] ORDER BY score DESC LIMIT 3");
+});
+
+app.MapPost("/addscore", (Coneflip coneflip, IDbConnection db) =>
+{
+    if (coneflip.Score == 0 || coneflip.Name == null || coneflip.video_link == null)
+    {
+        return Results.BadRequest();
+    }
+
+    // db.Execute("UPDATE [coneflip] SET [Content] = @Content WHERE [Id] = @Id", new
+    // {
+    //     Content = newOrUpdatedItem.Content,
+    //     Id  = newOrUpdatedItem.Id
+    // });
+
+    return Results.Ok();
+});
+
+app.Run();
+
+public class Coneflip
+{
+    public required string Name { get; set; }
+
+    public required int Score { get; set; }
+    public required string video_link { get; set; }
+}
